3a94cf0641ff5d20e01dd979090d2f24a482935a
dnestoff & Jack Noble <pair+dnestoff.winged115@devbootcamp.com>
2016-10-05 17:50:02 -0500

    Write test for is_admin and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe User  do
-  let(:user) { User.create!(username: "jack_noble", email: "jack@yahoo.com", password: "password", is_admin: false) }
+  let(:user) { User.create!(username: "jack_noble", email: "jack@yahoo.com", password: "password") }
 
   describe "attributes" do
     it "has a username" do
@@ -10,6 +10,14 @@ describe User  do
 
     it "has an email" do
       expect(user.email).to eq("jack@yahoo.com")
+    end
+
+    it "has a hashed password" do
+      expect(user.password_digest).to_not be user.password
+    end
+
+    it "has an admin status of false upon creation" do
+      expect(user.is_admin).to eq false
     end
   end
 end
