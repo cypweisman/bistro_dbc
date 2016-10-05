5d9568ada55df4df2d5757566eac2472666d224f
dnestoff & Jack Noble <pair+dnestoff.winged115@devbootcamp.com>
2016-10-05 18:49:40 -0500

    Add feature test for registering and add #create method for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,21 @@ class UsersController < ApplicationController
     @user = User.new
     render :new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      @errors = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :username, :email)
+  end
 end
--- spec/features/registering_feature_spec.rb
+++ spec/features/registering_feature_spec.rb
@@ -1,1 +1,23 @@
 require "rails_helper"
+
+feature 'registering for the site' do
+  scenario "the user clicks on register and is registered" do
+    visit '/users/new'
+    within_fieldset("Register") do
+      fill_in 'username', :with => "Jack"
+    end
+
+    within_fieldset("Register") do
+      fill_in 'email', :with => "jack.carmco@yahoo.com"
+    end
+
+    within_fieldset("Register") do
+      fill_in 'password', :with => "password"
+    end
+
+    click_button('Create')
+
+    visit '/'
+    expect(page).to have_content "logout"
+  end
+end
