4d5825088d672cf2d87cf1a713986520a180d678
Jack Noble <email": null>
2016-10-06 13:59:09 -0500

    Fix testdrive issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,13 @@ class User < ActiveRecord::Base
   after_save :default_admin_to_false
 
   def default_admin_to_false
-    self.is_admin = false
+    if self.is_admin == nil
+      self.is_admin = false
+    elsif self.is_admin == false
+      self.is_admin = false
+    else
+      self.is_admin = true
+    end
   end
 
 end
