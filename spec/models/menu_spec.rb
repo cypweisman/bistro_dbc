12f1e78f00b6ae08073890c589eee1b8325ac586
Jack Noble <email": null>
2016-10-06 16:18:06 -0500

    validations pass on Menu

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -18,4 +18,22 @@ describe Menu do
       expect(menu.user_id).to eq(1)
     end
   end
+
+  describe "validations" do
+    it "is invalid without a title" do
+      Menu.create(description: "So Great!", user_id: 1)
+      expect(Menu.count).to eq(0)
+    end
+
+    it "is invalid without a description" do
+      Menu.create(title: "Brunch", user_id: 1)
+      expect(Menu.count).to eq(0)
+    end
+
+    it "is invalid without a user id" do
+      Menu.create(title: "Brunch", description: "So Great!")
+      expect(Menu.count).to eq (0)
+    end
+  end
 end
+
