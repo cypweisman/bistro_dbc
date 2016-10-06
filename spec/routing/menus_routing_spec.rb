425f3aa7b36a00287f7db1cb2aec5725d529899a
Jack Noble <email": null>
2016-10-06 17:30:09 -0500

    Add test for menu post route

--- spec/routing/menus_routing_spec.rb
+++ spec/routing/menus_routing_spec.rb
@@ -2,9 +2,11 @@ require "rails_helper"
 
 RSpec.describe "Routing for menus", :type => :routing do
 
-  it "routes GET /users/1/menus/new to menues#new" do
-    expect(:get => "/users/1/menus/new").to route_to("menus#new", user_id: "1")
+  it "routes GET /users/1/menus/new to menus#new" do
+    expect(:get => "/users/1/menus/new").to route_to("menus#new", user_id: '1')
   end
 
-
+  it "routes POST /users/1/menus to menus#create" do
+    expect(:post => "/users/1/menus").to route_to("menus#create", user_id: '1')
+  end
 end
