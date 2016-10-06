57c28914e325919e41b65281356a014476aeb145
dnestoff <email": null>
2016-10-06 15:20:28 -0500

    All test for recipe passing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,25 @@
 class RecipesController < ApplicationController
 
+  def new
+    @user = User.find(params[:user_id])
+    render :new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      @error = @recipe.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :user_id, :directions, :prep_time, :category_id)
+  end
+
 end
--- spec/routing/recipes_routing_spec.rb
+++ spec/routing/recipes_routing_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 RSpec.describe "Routing to recipes", :type => :routing do
 
-  it "routes GET /recipes/new to recipes#new" do
-    expect(:get => "/recipes/new").to route_to("recipes#new")
+  it "routes GET /users/1/recipes/new to recipes#new" do
+    expect(:get => "/users/1/recipes/new").to route_to("recipes#new", :user_id => '1')
   end
 
-  it "routes POST /users/:id/recipes to recipes#create" do
-    expect(:post => "/users/:id/sessions").to route_to("recipes#create")
+  it "routes POST /users/:user_id/recipes to recipes#create" do
+    expect(:post => "/users/1/recipes").to route_to("recipes#create", :user_id => '1')
   end
 
 end
