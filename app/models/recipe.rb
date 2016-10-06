069be4cf581848ed3d0e5e247c6bed86115c7b93
dnestoff <email": null>
2016-10-06 11:14:43 -0500

    Write model validations and test for recipes and ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ActiveRecord::Base
+  has_many :ingredients
+  belongs_to :user
+  belongs_to :category
+
+  validates :name, :description, :prep_time, :directions, :user_id, presence: true
 
 end
