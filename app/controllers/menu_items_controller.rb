ea500a4669aa406e5cc6d4a77e4459e80c15d08e
Jack Noble <email": null>
2016-10-07 14:59:40 -0500

    Edit Delete item working

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -10,11 +10,27 @@ class MenuItemsController < ApplicationController
       @errors = "Error fix"
       render "/recipes/index"
     end
+  end
 
+  def update
+    @menu_item = MenuItem.find(params[:id])
+    if @menu_item.update_attributes(menu_item_params)
+      redirect_to menu_path(@menu_item.menu)
+    else
+      @menu = @menu_item.menu
+      @errors = @menu_item.errors.full_messages
+      render "/menus/show"
+    end
+  end
+
+  def destroy
+    @menu_item = MenuItem.find(params[:id])
+    @menu_item.destroy
+    redirect_to menu_path(@menu_item.menu)
   end
 
   def menu_item_params
-    params.require(:menu_item).permit(:recipe_id, :menu_id)
+    params.require(:menu_item).permit(:price, :description, :recipe_id, :menu_id)
   end
 
 end
