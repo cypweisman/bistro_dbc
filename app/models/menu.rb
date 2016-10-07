5d7b6edf18eeba47e7a22f23af2c00ddade68772
Jack Noble <email": null>
2016-10-07 11:02:40 -0500

    Add sort function for menu show

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -4,4 +4,9 @@ class Menu < ActiveRecord::Base
   has_many :recipes, through: :menu_items, source: :recipe
 
   validates :title, :description, :user_id, presence: true
+
+  def sort_by_category
+    sorted_items = self.menu_items.sort_by { |item| item.recipe.category.title}
+    sorted_items
+  end
 end
